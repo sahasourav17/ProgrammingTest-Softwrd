1200c4c22d0b013acb20fc532e2a3175af6a51bc
sahasourav17 <sahasourav1170@gmail.com>
2023-09-04 00:18:49 +0600

    add Map page

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import { Route, Router } from "svelte-routing";
   import Layout from "./lib/Layout.svelte";
+  import MapPage from "./lib/MapPage.svelte";
   import ChartPage from "./lib/chartPage/ChartPage.svelte";
 </script>
 
@@ -8,6 +9,7 @@
   <Router>
     <Route path="/" component={Layout} />
     <Route path="/chartpage" component={ChartPage} />
+    <Route path="/map-page" component={MapPage} />
   </Router>
 </main>
 
---
+++ src/lib/MapPage.svelte
@@ -0,0 +1,65 @@
+<!-- OpenLayersMap.svelte -->
+
+<script>
+  import { onMount } from "svelte";
+  import { Map, View } from "ol";
+  import TileLayer from "ol/layer/Tile";
+  import XYZ from "ol/source/XYZ";
+  import GeoJSON from "ol/format/GeoJSON";
+  import VectorLayer from "ol/layer/Vector";
+  import VectorSource from "ol/source/Vector";
+  import Style from "ol/style/Style";
+  import Fill from "ol/style/Fill";
+  import Stroke from "ol/style/Stroke";
+
+  let map;
+
+  onMount(() => {
+    map = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          }),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+
+    const vectorSource = new VectorSource({
+      format: new GeoJSON(),
+      url: "https://openlayers.org/en/v4.6.5/examples/data/geojson/countries.geojson",
+    });
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+      style: function (feature, res) {
+        if (feature.get("name") == "Bangladesh") {
+          return new Style({
+            stroke: new Stroke({
+              color: "#006a4e",
+              width: 2,
+            }),
+            fill: new Fill({
+              color: "rgba(0, 106, 78, 0.75)",
+            }),
+          });
+        }
+      },
+    });
+
+    map.addLayer(vectorLayer);
+  });
+</script>
+
+<div id="map" />
+
+<style>
+  #map {
+    width: 100%;
+    height: 100vh;
+  }
+</style>
